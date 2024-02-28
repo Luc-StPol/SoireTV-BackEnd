e69ff769f4d84ac5fcef49ce97f013ee54e938a9
Luc-StPol <saintpol.luc@gmail.com>
2024-02-28 17:57:44 +0100

    AddFavorite route ajouté

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,7 @@ const userCtrl = require ('../controllers/user.js')
 
 router.post('/signup', userCtrl.signup)
 router.post('/login', userCtrl.login)
+router.put('/addfavorite/:id', userCtrl.addFavorites)
 
 
 module.exports= router
