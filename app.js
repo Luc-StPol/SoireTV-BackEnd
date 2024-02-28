643dc922936e5f5d10b3271ad8b153166db351e7
Luc-StPol <saintpol.luc@gmail.com>
2024-02-28 20:19:30 +0100

    moddif app

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+
+
 const mongoose = require('mongoose')
 const express = require('express')
 
 const userRoutes = require('./routes/user.js')
-
+const auth = require('./middleware/auth.js')
 
 const app = express()
 
